b2e27f90c5499d3b5aa8d18e9be172866d0387da
lincoln <yifan_le@163.com>
2019-01-23 19:42:23 -0600

    1.23

--- student_code.py
+++ student_code.py
@@ -24,7 +24,12 @@ class KnowledgeBase(object):
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
         print("Asserting {!r}".format(fact))
-        
+        if isinstance(fact, Fact) and fact not in self.facts:
+            self.facts.append(fact)
+            print('Asserting succeeded')
+        else:
+            print('Asserting failed')
+
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
 
@@ -35,3 +40,15 @@ class KnowledgeBase(object):
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
         print("Asking {!r}".format(fact))
+        lob = ListOfBindings()
+        for f in self.facts:
+            binding = match(fact.statement, f.statement)
+            if binding:
+                #print(binding)
+                lob.add_bindings(binding)
+        if len(lob) != 0:
+            return lob
+        return False
+
+
+
